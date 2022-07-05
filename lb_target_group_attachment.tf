16983fd1340a97329877fd40a138749242fca8ab
Andrew Sidorkin <sidorkin.andrew@gmail.com>
2022-07-05 20:11:13 +0000

    added depend_on

--- lb_target_group_attachment.tf
+++ lb_target_group_attachment.tf
@@ -2,4 +2,5 @@ resource "aws_lb_target_group_attachment" "flask_api_tg_attach" {
   count            = length(data.aws_instances.launched_by_asg.ids)
   target_group_arn = aws_lb_target_group.flask_api_tg.id
   target_id        = data.aws_instances.launched_by_asg.ids[count.index]
+  depends_on = [data.aws_instances.launched_by_asg]
 }
